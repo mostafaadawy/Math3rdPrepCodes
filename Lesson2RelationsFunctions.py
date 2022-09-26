2e08498c0d92c9964f7d42d3887d91dee8cfc1f7
mostafaadawy <mostafa_adawy@ymail.com>
2022-09-26 19:05:29 +0200

    lesson2 Relations and  functions

---
+++ Lesson2RelationsFunctions.py
@@ -0,0 +1,20 @@
+#Three lines to make our compiler able to draw:
+import sys
+
+import matplotlib.pyplot as plt
+
+#X={1,2,3}
+#Y={1,2,3}
+X=range(1,10)
+Y=range(1,10)
+#a R b y=x
+for x in X:
+  for y in Y:
+    #if x + y==10:
+    if x == y:
+      plt.scatter([x],[y])
+  
+plt.show()
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
