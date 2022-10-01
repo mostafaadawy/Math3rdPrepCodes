6221a11721622eb913c809a3137579c1c3f08744
mostafaadawy <mostafa_adawy@ymail.com>
2022-10-01 11:12:38 +0200

    added fns

--- Lesson2RelationsFunctions.py
+++ Lesson2RelationsFunctions.py
@@ -6,12 +6,14 @@ import matplotlib.pyplot as plt
 #X={1,2,3}
 #Y={1,2,3}
 X=range(1,10)
-Y=range(1,10)
+Y=range(-10,10)
 #a R b y=x
 for x in X:
   for y in Y:
     #if x + y==10:
-    if x == y:
+    #if x == y:
+    #if x==abs(y):
+    if x==0.5*y:
       plt.scatter([x],[y])
   
 plt.show()
