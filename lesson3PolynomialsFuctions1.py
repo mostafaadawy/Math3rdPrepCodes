5efb1a141ebbe3fca1b2083f79b8a8e96ef96efb
mostafaadawy <mostafa_adawy@ymail.com>
2022-10-03 18:43:16 +0200

    lesson 3 1

---
+++ lesson3PolynomialsFuctions1.py
@@ -0,0 +1,29 @@
+#Three lines to make our compiler able to draw:
+import sys
+
+import matplotlib.pyplot as plt
+
+
+X={-1,0,1}
+Y={0,-1,-2}
+
+def x2minusone(x):
+    return x*x-1
+
+# 1-get the projections or range
+images=set()
+for x in X:
+    #images.add(x*x-1)
+    images.add(x2minusone(x))
+print(images)
+
+print('f set ={',end='')
+for x in X:
+    print('(',x,',',x2minusone(x),'),',end='')
+    plt.scatter([x],[x2minusone(x)])
+print('}')
+
+plt.show()
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
