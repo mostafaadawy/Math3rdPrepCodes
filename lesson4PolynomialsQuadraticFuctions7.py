baded722a7771ad6fcda247e3dcf1cc9a0e9d8c3
mostafaadawy <mostafa_adawy@ymail.com>
2022-10-03 18:48:26 +0200

    lesson4PolynomialsQuadraticFuctions7.py

---
+++ lesson4PolynomialsQuadraticFuctions7.py
@@ -0,0 +1,38 @@
+#Three lines to make our compiler able to draw:
+import sys
+import math
+import matplotlib.pyplot as plt
+
+
+#X={-3,-2,-1,0,1,2,3}
+#X=range(-3,4)
+X=[-3,-2,-1,0,1,2,3]
+
+
+def quadratic(x):
+    return x*x
+
+
+# get the projections or f(x)
+#fx=set()
+fx=[]
+
+plt.title("quadratic function")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+
+for x in X:
+    #fx.add(quadratic(x))
+    fx.insert(0,quadratic(x))
+    plt.scatter([x],[quadratic(x)],s=50)
+
+
+plt.grid()
+print('X=',X)
+print('f(x)=',fx)
+plt.show()
+
+
+#Two  lines to make our compiler able to draw:
+plt.savefig(sys.stdout.buffer)
+sys.stdout.flush()
