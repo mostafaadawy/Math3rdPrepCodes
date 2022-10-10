6380f14dc44d9fa631a556686c8af8e7784d7fc2
mostafaadawy <mostafa_adawy@ymail.com>
2022-10-10 15:59:58 +0200

    ed

--- algebraU1Lesson4/withsmoothing.py
+++ algebraU1Lesson4/withsmoothing.py
@@ -1,19 +1,17 @@
-#Three lines to make our compiler able to draw:
 import sys
-
 import math
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import make_interp_spline as intr
 
 
-X=np.array([-4,-3,-2,-1,0,1,2])
+X=np.array([-2,-1,0,1,2,3,4])
 
 
-def fn(x):
-    return (x*x)+(2*x)-3
+def fn(x): 
+    return (x*x)-(2*x)-3
 
-# 1-get the projections or range
+
 images=set()
 fx=[]
 print('f ={',end='')
@@ -25,20 +23,18 @@ print('}')
 print('range=',images)
 print('X=',X)
 print('f(x)=',fx)
+plt.title("quadratic function")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.grid()
 
 XYInter = intr(X, fx)
 X_ = np.linspace(X.min(), X.max(), 500)
 fx_ = XYInter(X_)
-
 plt.plot(X_,fx_)
-plt.title("quadratic function")
-plt.xlabel("x-axis")
-plt.ylabel("y-axis")
+
+
 plt.show()
-#Two  lines to make our compiler able to draw:
+
 plt.savefig(sys.stdout.buffer)
 sys.stdout.flush()
-
-
-
-
